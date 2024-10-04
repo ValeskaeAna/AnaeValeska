061412dde3d536d945c4c3ea54aa6e1560e840d6
ValeskaeAna <159259271+ValeskaeAna@users.noreply.github.com>
2024-10-04 10:47:17 -0300

    Add files via upload

--- graficos/informacoesGlobais.js
+++ graficos/informacoesGlobais.js
@@ -3,13 +3,14 @@ const url = 'https://raw.githubusercontent.com/guilhermeonrails/api/main/dados-g
 async function visualizarInformacoesGlobais() {
     const res = await fetch(url)
     const dados = await res.json()
-    console.log(dados);
+    const pessoasConectadas = (dados.total_pessoas_conectadas / 1e9)
+    
     const paragrafo = document.createElement('p')
     paragrafo.classList.add('graficos-container__texto')
-    paragrafo.innerHTML = `Você sabia que o mundo tem ${dados.total_pessoas_mundo} de pessoas e que aproximadamente ${dados.total_pessoas_conectadas} estão conectadas em alguma rede social e passam em média ${dados.tempo_medio} horas conectadas.`
-    console.log(paragrafo)
 
-    const container = document.getElementById('')
+    paragrafo.innerHTML = `Você sabia que o mundo tem <span>${dados.total_pessoas_mundo}</spam> de pessoas e que aproximadamente <span>${dados.total_pessoas_conectadas}</span> estão conectadas em alguma rede social e passam em média <span>${dados.tempo_medio} </span>horas conectadas.`
+    const container = document.getElementById('graficos-conteiner')
+    container.appendChild(paragrafo)
 }
 
 visualizarInformacoesGlobais()
