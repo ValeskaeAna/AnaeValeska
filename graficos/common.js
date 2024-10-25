dcb6632277122b7c0cf8ad1765d358bb70aa2597
ValeskaeAna <159259271+ValeskaeAna@users.noreply.github.com>
2024-10-25 10:45:55 -0300

    Add files via upload

--- graficos/common.js
+++ graficos/common.js
@@ -8,4 +8,15 @@ const tickConfig = {
     color: getCSS('--primary-color')
 }
 
+function criarGrafico (data, layout) {
+    const grafico = document.createElement('div');
+    grafico.className = 'grafico';
+    document.getElementById('graficos-container').appendChild(grafico);
+    const Config = {
+        responsiv: true,
+        displayayModeBar: false
+      }
+    Plotly.newPlot (grafico,data, layout, Config);
+}
+
 export {getCSS, tickConfig}
